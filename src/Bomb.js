4d9974527373cd7304f0f448bd67a5328ea371cb
Yan C Lin <yonalin1@gmail.com>
2019-07-24 21:31:40 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,33 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor (props) {
+        super ();
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    };
+
+    render () {
+        let timer = this.state.secondsLeft;
+        if (timer > 0) {
+            return tickerText(timer)
+        } else {
+            return (
+            <div className='bomb'>
+                Boom!
+            </div>
+            )
+        }
+    };
+};
+
+function tickerText(timer) {
+    return (
+        <div className='bomb'>
+          {timer} seconds left before I go boom!
+        </div>
+    )
+};
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+    constructor () {
+        super ();
+        this.state = {
+            currentSlideIndex: 0
+        }
+    };
+
+    render () {
+        return (
+            <div className='image-slider'>
+            <h2>I am on slide {this.state.currentSlideIndex}</h2>
+            </div>
+        )
+    }
+};
+
+export default ImageSlider;
